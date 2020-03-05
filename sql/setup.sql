7fb268e41a771c604ca91327126c899ab802692a
Jason Carvalho <jase@jasoncarvalho.com>
2020-03-05 15:46:36 +0000

    DB setup for FILE table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,17 +1,20 @@
 create table if not exists file
 (
-    id            int default 0 not null
+    id                int auto_increment
         primary key,
-    dataset_id    int           not null,
-    title         varchar(64)   not null,
-    description   varchar(1024)  null,
-    filename      varchar(64)   not null,
-    location      varchar(64)   not null,
-    date_created  datetime      null,
-    date_modified datetime      null,
+    dataset_id        int           not null,
+    title             varchar(64)   not null,
+    description       varchar(1024) null,
+    filename          varchar(64)   not null,
+    filename_original varchar(64)   not null,
+    file_type         varchar(64)   null,
+    file_size         int           null,
+    date_created      datetime      null,
+    date_modified     datetime      null,
     constraint file_dataset_id_fk
         foreign key (dataset_id) references dataset (id)
 );
 
 create index file_dataset_id_index
     on file (dataset_id);
+
